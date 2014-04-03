59b954bc34250997425726a41bc35ec8888594d0
Matthew Hruz <matthew.hruz@gmail.com>
2014-04-03 04:52:56 -0500

    Got cart to move again
    Thinking about having the cart move again, so I implemented that. It is
    leading to indexOutOfBounds exceptions though. Changes to moveCart
    should allow the cart to speed up naturally in a more organic way.

--- Developer Project/Assets/Scripts/MoveCart.cs
+++ Developer Project/Assets/Scripts/MoveCart.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MoveCart : MonoBehaviour {
 
-	private float maxSpeed = 15f;
+	public float maxSpeed = 25f;
 	
 	// Use this for initialization
 	void Start () {
@@ -12,6 +12,13 @@ public class MoveCart : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		rigidbody2D.velocity = new Vector3(maxSpeed, 0f, 0f);
+		if (rigidbody2D.velocity.x < maxSpeed) {
+			Debug.Log("trying to add force");
+			rigidbody2D.AddForce(new Vector2(5f, 0f));
+		} else {
+			rigidbody2D.velocity = new Vector2(maxSpeed, 0f);
+		}
+		Debug.Log ("Fixed update with x velocity: " + rigidbody2D.velocity.x);
+		//rigidbody2D.velocity = new Vector3(maxSpeed, 0f, 0f);
 	}
 }
