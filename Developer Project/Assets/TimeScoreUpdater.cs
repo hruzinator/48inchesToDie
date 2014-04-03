a86a82d446f4986873e39ee1d0b93260167de92a
Matthew Hruz <matthew.hruz@gmail.com>
2014-04-02 20:55:36 -0500

    Added GUIText to track score
    Score is currently set to time

---
+++ Developer Project/Assets/TimeScoreUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScoreUpdater : MonoBehaviour {
+
+	private GameObject cf;
+	private float scoreTime;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject.FindWithTag("MainCamera").t
+		//gameObject.GetComponent<GUIText>().transform
+		cf = GameObject.Find("cartfront");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cf.GetComponent<PlayerCollider> ().isDead) {
+			gameObject.GetComponent<GUIText>().text = "Final score time: " + scoreTime.ToString();
+		}
+		else{
+			scoreTime = Time.realtimeSinceStartup;
+			gameObject.GetComponent<GUIText>().text = "Time: " + scoreTime.ToString();
+		}
+	}
+}
